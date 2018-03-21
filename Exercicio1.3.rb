23a8e5f48693435212f7f030e1fee6d92f927698
Ryuutaro <35067106+Ryuutaro@users.noreply.github.com>
2018-03-21 11:03:04 -0300

    Add files via upload

---
+++ Exercicio1.3.rb
@@ -0,0 +1,13 @@
+print "Digite a sua primeira nota:"
+n1 = Float(gets)
+print "Digite a sua segunda nota:"
+n2 = Float(gets)
+print "Digite a sua terceira nota:"
+n3 = Float(gets)
+print "Digite sua quarta e última nota:"
+n4 = Float(gets)
+
+media = (n1+n2+n3+n4)/4
+
+print "A sua média de notas é: #{media}"
+gets
